1712f95aae70cb8b8c91bb9a0a6fb0cd9da7a422
janburian <jburian@students.zcu.cz>
2021-12-10 09:22:01 +0100

    Create detectron2_cells.py

---
+++ detectron2_cells.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#Detectron2 - cells.ipynb
+
+#Original file is located at https://colab.research.google.com/drive/1rCLduoeFC_UKN5MEKF77nvPVkbxOzt1x
+
+#**Usage of Detectron2**
+
+# Install detectron2
+# Some basic setup
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+from pathlib import Path
+import os
+
+scratchdir = os.getenv('SCRATCHDIR', ".")
+
+input_data_dir = Path(scratchdir) / 'data/orig/'
+outputdir = Path(scratchdir) / 'data/processed/'
+
+
+
+from detectron2.data.datasets import register_coco_instances
+register_coco_instances("cells", {},  str(input_data_dir / "data/trainval.json"), str(input_data_dir / "data/images"))
+
+cells_metadata = MetadataCatalog.get("cells")
+dataset_dicts = DatasetCatalog.get("cells")
+
+"""To verify the data loading is correct, let's visualize the annotations of randomly selected samples in the training set:
+
+
+"""
+
+import random
+
+#for d in random.sample(dataset_dicts, 3):
+for d in dataset_dicts:
+    img = cv2.imread(d["file_name"])
+    visualizer = Visualizer(img[:, :, ::-1], metadata=cells_metadata, scale=0.5)
+    vis = visualizer.draw_dataset_dict(d)
+    cv2.imwrite(f'{outputdir / "vis_train" / d["file_name"]}', vis.get_image()[:, :, ::-1])
+
+
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+import os
+
+cfg = get_cfg()
+cfg.merge_from_file("./detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.DATASETS.TRAIN = ("cells",)
+cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"  # initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.02
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough, but you can certainly train longer
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1 class (cells)
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
+cfg.DATASETS.TEST = ("cells", )
+predictor = DefaultPredictor(cfg)
+
+
+from detectron2.utils.visualizer import ColorMode
+
+for d in random.sample(dataset_dicts, 3):
+    im = cv2.imread(d["file-name"])
+    outputs = predictor(im)
+    v = Visualizer(im[:, :, ::-1],
+                    metadata=cells_metadata,
+                    scale=3,
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
+    )
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imwrite(f'{outputdir / "vis_predictions" / d["file_name"]}', vis.get_image()[:, :, ::-1])
